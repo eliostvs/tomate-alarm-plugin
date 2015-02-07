6255395cc4f25d878bb515fe922d77cb5bbf7241
Elio E. Duarte <elio.esteves.duarte@gmail.com>
2015-02-07 01:12:22 -0200

    docker tasks

--- pavement.py
+++ pavement.py
@@ -16,8 +16,8 @@ DATA_PATH = ROOT_PATH / 'data'
 PLUGIN_PATH = DATA_PATH / 'plugins'
 
 
-@needs(['test'])
 @task
+@needs(['test'])
 def default():
     pass
 
@@ -35,6 +35,17 @@ def test(options):
     os.environ['PYTHONPATH'] = '%s:%s' % (TOMATE_PATH, PLUGIN_PATH)
     os.environ['XDG_DATA_DIRS'] = str(DATA_PATH)
     sh('nosetests --cover-erase --with-coverage tests.py')
+
+
+@task
+@needs(['docker_rmi', 'docker_build', 'docker_run'])
+def docker_test():
+    pass
+
+
+@task
+def docker_rmi():
+    sh('docker rmi eliostvs/tomate-alarm-plugin', ignore_error=True)
 
 
 @task
