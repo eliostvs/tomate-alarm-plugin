fe6eba13e0bbfdff46408343374136fa5a06a13f
Elio Esteves Duarte <elio.esteves.duarte@gmail.com>
2019-09-08 12:17:54 -0300

    Blacknify file.

--- test_plugin.py
+++ test_plugin.py
@@ -3,7 +3,7 @@ import pytest
 
 from unittest.mock import patch, Mock
 
-gi.require_version('Gst', '1.0')
+gi.require_version("Gst", "1.0")
 
 from gi.repository import Gst
 from tomate.graph import graph
@@ -11,20 +11,20 @@ from tomate.graph import graph
 
 def setup_module():
     graph.register_instance(
-        'tomate.config',
-        Mock(**{'get_media_uri.return_value': '/usr/share/tomate/media/alarm.ogg'})
+        "tomate.config",
+        Mock(**{"get_media_uri.return_value": "/usr/share/tomate/media/alarm.ogg"}),
     )
 
 
 @pytest.fixture(autouse=True)
 def config(mocker):
     mock = mocker.Mock()
-    graph.register_instance('tomate.config', mock)
+    graph.register_instance("tomate.config", mock)
     return mock
 
 
 @pytest.fixture
-@patch('alarm_plugin.Gst.ElementFactory.make')
+@patch("alarm_plugin.Gst.ElementFactory.make")
 def plugin(factory_make):
     from alarm_plugin import AlarmPlugin
 
@@ -39,18 +39,18 @@ def message(mocker):
     return mock
 
 
-@patch('alarm_plugin.Gst.ElementFactory.make')
+@patch("alarm_plugin.Gst.ElementFactory.make")
 def test_create_playbin(make):
     from alarm_plugin import AlarmPlugin
 
     plugin = AlarmPlugin()
 
-    make.assert_called_once_with('playbin', None)
+    make.assert_called_once_with("playbin", None)
 
     plugin.player.set_state.assert_called_once_with(Gst.State.NULL)
 
 
-file_path = '/usr/share/tomate/media/alarm.ogg'
+file_path = "/usr/share/tomate/media/alarm.ogg"
 
 
 def test_ring_with_default_alarm_file(plugin, config):
@@ -60,16 +60,18 @@ def test_ring_with_default_alarm_file(plugin, config):
         if section == CONFIG_SECTION_NAME and option == CONFIG_OPTION_NAME:
             return None
 
-        return 'Error'
+        return "Error"
 
     config.get.side_effect = side_effect
-    config.get_media_uri.side_effect = lambda file: file_path if file == 'alarm.ogg' else None
+    config.get_media_uri.side_effect = (
+        lambda file: file_path if file == "alarm.ogg" else None
+    )
 
     plugin.player.reset_mock()
 
     plugin.ring()
 
-    plugin.player.set_property.assert_called_once_with('uri', file_path)
+    plugin.player.set_property.assert_called_once_with("uri", file_path)
     plugin.player.set_state.assert_called_once_with(Gst.State.PLAYING)
 
 
@@ -86,7 +88,7 @@ def test_ring_with_custom_alarm_file(plugin, config):
 
     plugin.ring()
 
-    plugin.player.set_property.assert_called_once_with('uri', file_path)
+    plugin.player.set_property.assert_called_once_with("uri", file_path)
     plugin.player.set_state.assert_called_once_with(Gst.State.PLAYING)
 
 
@@ -99,7 +101,7 @@ def test_player_should_change_state_to_null(plugin, message):
 
 def test_player_should_change_state_to_null_when_error(plugin, message):
     message.type = Gst.MessageType.ERROR
-    message.parse_error.return_value = ('', '')
+    message.parse_error.return_value = ("", "")
 
     plugin.player.reset_mock()
 
